0b7f41216f7218da36ac175d512c490b2dea0e01
chainlinn <94969625+chainlinn@users.noreply.github.com>
2026-06-08 20:18:47 +0800

    hook updates

--- forge-cli.rb
+++ forge-cli.rb
@@ -2,7 +2,7 @@ class ForgeCli < Formula
   desc "一键创建 GitHub + Docker Hub 仓库，生成 CI/CD 部署流水线"
   homepage "https://github.com/chainlinn/forge"
   url "https://github.com/chainlinn/forge/releases/download/v0.1.0/forge-v0.1.0.tar.gz"
-  sha256 "63d0354e07fac6bdc13e68e9ea4db7af79f14467040d61f05d8fe2d48e175978"
+  sha256 "228278b5169ca3aadb7fb55dfd0fd131235e1897b49238bac5cfbe5f148d037a"
   license "MIT"
 
   conflicts_with "forge", because: "both ship a `forge` binary"
